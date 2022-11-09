c0c91649fb0b89681085e73cc0e8bff567ba3ece
mollahdev <mollah.dev@gmail.com>
2022-11-09 09:46:45 +0600

    setup laravel mix

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php  
+    // document open
+    include './partials/document-open.php'; 
+    // navbar
+    include './partials/navbar.php';
+?>
+
+<h1>
+    About page
+</h1>
+
+
+<?php include './partials/document.close.php' ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php  
+    // document open
+    include './partials/document-open.php'; 
+    // navbar
+    include './partials/navbar.php';
+?>
+
+<h1>
+    Home page
+</h1>
+
+
+<?php include './partials/document.close.php' ?>
