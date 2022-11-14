d713d938dda118622039c7ee571c73fef8b179b8
mollahdev <mollah.dev@gmail.com>
2022-11-14 10:11:44 +0600

    scss class 1$

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php  
     // document open
     include './partials/document-open.php'; 
-    // navbar
-    include './partials/navbar.php';
 ?>
 
 <h1>
     Home page
 </h1>
 
-
+<div class="flag">
+    <div class="round"></div>
+</div>
 <?php include './partials/document.close.php' ?>
