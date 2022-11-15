16733a75de662b01ce30ffbcf15cf8d94b9deb95
mollahdev <mollah.dev@gmail.com>
2022-11-15 09:42:20 +0600

    added banner css

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php  
     // document open
-    include './partials/document-open.php'; 
+    include './partials/document-open.php';
+    include './partials/navbar.php';
 ?>
 
-<h1>
-    Home page
-</h1>
+<section class="demo-banner text-center">
+    <div class="overlay">
+        <div class="container">
+            <picture>
+                <img loading="lazy" src="./images/common/logo.webp" alt="logo">
+            </picture>
+            <h1 class="page-title">CharityPress</h1>
+            <h3 class="page-description">CharityPress - Charity Fundraising HTML Template</h3>
+        </div>
+    </div>
+</section>
 
-<div class="flag">
-    <div class="round"></div>
-</div>
 <?php include './partials/document.close.php' ?>
