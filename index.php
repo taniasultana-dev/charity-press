f3894cef259d7d83aeb089361c87493355780ac1
mollahdev <mollah.dev@gmail.com>
2022-11-16 09:41:25 +0600

    BEM method

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
     include './partials/document-open.php'; 
 ?>
 
-<h1>
-    Home page
-</h1>
+<section class="banner banner--demo">
+    <div class="overlay">
+        <div class="container text-center text-white">
+            <picture>
+                <img class="banner__img" src="./images/logos/logo.webp" alt="site-logo">
+            </picture>
+            <h1 class="banner__title">CharityPress</h1>
+            <div class="banner__group-btn">
+                <a class="banner__primary-btn" href="#">buy theme</a>  
+                <a class="banner__secondary-btn" href="#">demos</a>  
+            </div>
+        </div>
+    </div>
+</section>
 
-<div class="flag">
-    <div class="round"></div>
-</div>
 <?php include './partials/document.close.php' ?>
