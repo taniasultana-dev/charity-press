7f4fe7bdb0f5a2192cb5a00a5b4642bf2afdb861
taniasultana-dev <taniasultana.dev@gmail.com>
2022-11-16 18:43:59 +0600

    banner.done

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php  
     // document open
     include './partials/document-open.php'; 
-    // navbar
-    include './partials/navbar.php';
+
+    
 ?>
 
-<h1>
-    Home page
-</h1>
+<section class="banner banner--demo">
+    <div class="banner__overlay">
+       <div class="container text-center text-white">
+           <picture>
+            <img class="banner__img mb-30" lodding="lazy" src="./images/logos/logo.webp" alt="" srcset="">
+            </picture>
+            <h1 class="banner__title mb-30">CharityPress</h1>
+            <h4 class="banner__sub-title mb-30">CharityPress - Charity Fundraising HTML Template</h4>
+            <div class="group-btn">
+                <a class="primary-btn text-decoration-none text-white rounded-pill me-3" href="#">Buy theme</a>
+                <a class="secondary-btn text-decoration-none text-white rounded-pill" href="#">demos</a>
+            </div>
+       </div>
+            
+    </div>
+</section>
+
+
+
 
 
 <?php include './partials/document.close.php' ?>
--- partials/document-open.php
+++ partials/document-open.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/app.css">
+    <title>CharityPress - Charity Fundraising HTML Template</title>
+     <link rel="shortcut icon" href="./images/logos/favicon.webp" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+     <link rel="shortcut icon" href="./images/icon/favicon.ico" type="image/x-icon">
+     <link rel="stylesheet" href="./css/app.css">
 </head>
 <body>
