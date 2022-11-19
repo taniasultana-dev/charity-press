883e366341c8cd883213b9d1a20062412d8ab0f4
taniasultana-dev <taniasultana.dev@gmail.com>
2022-11-19 15:38:48 +0600

    banner-responsive-done

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
            <picture>
             <img class="banner__img mb-30" lodding="lazy" src="./images/logos/logo.webp" alt="" srcset="">
             </picture>
-            <h1 class="banner__title mb-30">CharityPress</h1>
-            <h4 class="banner__sub-title mb-30">CharityPress - Charity Fundraising HTML Template</h4>
+            <h1 class="banner__title text-lg weight-lg l-height mb-30">CharityPress</h1>
+            <h4 class="banner__sub-title text-md weight-m m-height mb-40">CharityPress - Charity Fundraising HTML Template</h4>
             <div class="group-btn">
-                <a class="primary-btn text-decoration-none text-white rounded-pill me-3" href="#">Buy theme</a>
-                <a class="secondary-btn text-decoration-none text-white rounded-pill" href="#">demos</a>
+                <a class="primary-btn text-decoration-none text-white d-inline-block text-sm weight-lg rounded-pill me-3 text-uppercase" href="#">Buy theme</a>
+                <a class="secondary-btn text-decoration-none text-white d-inline-block text-sm weight-lg rounded-pill text-uppercase" href="#">demos</a>
             </div>
        </div>
             
