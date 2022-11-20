fafa049716eedb81372d83102ee3269d8cb585ef
mollahdev <mollah.dev@gmail.com>
2022-11-20 10:15:59 +0600

    added css variable into banner

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
                 <img class="banner__img" src="./images/logos/logo.webp" alt="site-logo">
             </picture>
             <h1 class="banner__title">CharityPress</h1>
+            <p class="banner__description">CharityPress - Charity Fundraising HTML Template</p>
             <div class="banner__group-btn">
-                <a class="banner__primary-btn" href="#">buy theme</a>  
-                <a class="banner__secondary-btn" href="#">demos</a>  
+                <a class="banner__primary-btn" href="#"><span>buy theme</span></a>  
+                <a class="banner__secondary-btn" href="#">
+                    <span>demos</span>
+                </a>  
             </div>
         </div>
     </div>
