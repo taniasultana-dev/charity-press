187aa6530ce32d0787402db3a3763d8cfe9791d6
mollahdev <mollah.dev@gmail.com>
2022-11-29 10:14:56 +0600

    added owl carousel

--- partials/document-open.php
+++ partials/document-open.php
@@ -9,6 +9,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./lib/css/owl.carousel.min.css">
     <link rel="stylesheet" href="./css/app.css">
+
+    <script src="./lib/js/jquery-3.6.1.min.js" defer></script>
+    <script src="./lib/js/owl.carousel.min.js" defer></script>
+    <script src="./js/script.js" defer></script>
 </head>
 <body>
