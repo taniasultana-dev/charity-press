44e1cd182d9c2897353d631503b2bc632ccd4423
taniasultana-dev <taniasultana.dev@gmail.com>
2023-01-28 15:42:03 +0600

    responsive done

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,7 +2,7 @@
   <div class="container">
     <a class="navbar-brand charity-navbar__brand" href="#">
       <picture>
-        <img width="150" height="77" loading="lazy" src="./../images/logos/logo-black.png" alt="logo">
+        <img width="150" height="77" loading="lazy" src="./images/logos/logo-black.png" alt="logo">
       </picture>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,36 +10,67 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-3">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           HOME
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="home-1.php">HOME-1</a></li>
+            <li><a class="dropdown-item" href="home-2.php">HOME-2</a></li>
+            <li><a class="dropdown-item" href="home-3.php">HOME-3</a></li>
+          </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#"> ABOUT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CAUSES</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            EVENTS
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">EVENT</a></li>
+            <li><a class="dropdown-item" href="#">EVENT SINGLE</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            pages
+            BLOG
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">BLOG</a></li>
+            <li><a class="dropdown-item" href="#">BLOG-SINGLE</a></li>
           </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          PAGES
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">DONATE NOW</a></li>
+            <li><a class="dropdown-item" href="#">FAQ</a></li>
+            <li><a class="dropdown-item" href="#">MISSON</a></li>
+            <li><a class="dropdown-item" href="#">PORTFOLIO</a></li>
+            <li><a class="dropdown-item" href="#">PRICING</a></li>
+            <li><a class="dropdown-item" href="#">SERVICE</a></li>
+            <li><a class="dropdown-item" href="#">TEAM</a></li>
+            <li><a class="dropdown-item" href="#">VOLUNTEER</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTACT</a>
         </li>
       </ul>
     </div>
-    <div class="ml-auto">
-      <button>Button</button>
+    <div class="charity-btn ml-auto">
+      <a class="charity-navbar__primary-btn" href="#">
+        <span>
+          <i class="fa-solid fa-heart"></i>
+          <span> Donate Now</span>
+        </span>
+      </a>  
     </div>
   </div>
 </nav>
